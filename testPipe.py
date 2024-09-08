cb9d8a9d2a7a25ab756b28c7e8c7b6a1bc82f5e7
Ashwini12Anand <ashwini.trainingbasket@gmail.com>
2024-09-09 00:22:03 +0530

    Initial commit

---
+++ testPipe.py
@@ -0,0 +1,14 @@
+
+
+class Test:
+    def __init__(self):
+        pass
+    
+    def process(self):
+        a = 15
+        b = 10
+        print(a+b)
+
+if __name__=="__main__":
+    obj = Test()
+    obj.process()
